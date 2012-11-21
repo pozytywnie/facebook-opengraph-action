a5b27e341da91aa0123f10c779074dd33f304e98
Piotr Maliński <piotr@pozytywnie.pl>
2012-11-21 16:44:55 +0100

    Create facebook-opengraph-action application.
    Change-Id: Ic51a5f104dbbff15fe9e36f6943c7aa0635cccd2

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='facebook-opengraph-action',
+    version='0.1',
+    maintainer="Tomasz Wysocki",
+    maintainer_email="tomasz@wysocki.info",
+    packages=find_packages(),
+    include_package_data=True,
+)
